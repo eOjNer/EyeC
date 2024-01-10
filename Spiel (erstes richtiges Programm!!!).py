9b8b541147f031e5e6b417b9f13c351f0e004c37
eOjNer <lle-redips@web.de>
2024-01-10 10:39:10 +0100

    Spiel am 10/1/2024

---
+++ Spiel (erstes richtiges Programm!!!).py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 10 09:16:12 2024
+
+@author: Administrator
+"""
+O = 'O'
+d = 'd'
+x = 0
+s = (f'\nSchade, beim nächsten mal schaffst du mehr als 0! Dein Rank ist {O}!')
+import time
+import random
+
+def timeAndInput():
+    start = time.time()
+    inp = input()
+    end = time.time()
+    return inp, end-start
+
+Words = {1 : 'Hallo',
+         2 : 'Bach',
+         3 : 'Flugzeug',
+         4 : 'Haus',
+         5 : 'Rotebeete'}
+
+while True:
+    RANDOM = random.randint(1,5)
+    print(Words[RANDOM])
+    input1, d = timeAndInput()
+    if input1 != Words[RANDOM] or d > len(Words[RANDOM]):
+        print(s)
+        break
+    else:
+        x = x + 1
+         
+    if x == 0:
+        O = 'F'
+    if x < 3 and x > 0:
+        O = 'D'
+    if O == 'D':
+        s = f'\nMit {x} Punkten und einem Rank {O}, schon mal mein guter anfang ;D!'
+    if x < 5 and x > 3:
+        O = 'C'
+    if O == 'C':
+        s = f'\nRank {O}, schon mit {x}? Fasst schon Rank B und dann ist es nur noch ein Katzensprung bis Rank A!'
+    if x < 8 and x > 5:
+        O = 'B'
+    if O == 'B':
+        s = f'\n{x} Punkte??? Es sind ja nur noch {10 - x} Punkte von deinem Rank B zum unglaublichen Rank A!!!'
+    if x >= 10:
+        O = 'S'
+    if O == 'S':
+        s = f'\nWOW!! Rank {O}!! Jetzt beginnt der Weg zu Welt Rekord! Das sind 561796327157 Punkte!! Du brauchst also nur noch {x-561796327157} Punkte!!!!!!'
+         
+         
--- morse1.py
+++ morse1.py
@@ -4,45 +4,7 @@ Spyder Editor
 
 This is a temporary script file.
 """
-import time
-Morsebuchstaben = input()
-
-mcodes = {'a' : [1,2],
-        'b' : [2,1,1,1],
-        'c' : [2,1,2,1],
-        'd' : [2,1,1],
-        'e' : [1],
-        'f' : [1,1,2,1],
-        'g' : [2,2,1],
-        'h' : [1,1,1,1],
-        'i' : [1,1],
-        'j' : [1,2,2,2],
-        'k' : [2,1,2],
-        'l' : [1,2,2,2],
-        'm' : [2,2],
-        'n' : [2,1],
-        'o' : [2,2,2],
-        'p' : [1,2,2,1],
-        'q' : [2,2,1,2],
-        'r' : [1,2,1],
-        's' : [1,1,1],
-        't' : [2],
-        'u' : [1,1,2],
-        'v' : [1,1,1,2],
-        'w' : [1,2,2],
-        'x' : [2,1,1,2],
-        'y' : [2,1,2,2],
-        'z' : [2,2,1,1],}
-
-def blink(t):
-    print("...led on.")
-    time.sleep(t)
-    print("...led off.")
-    time.sleep(1)
-
-
-for buchstabe in Morsebuchstaben:
-    a = mcodes[buchstabe]
-    for t in a:
-        blink(t)
-            
+ha = 'hallo'
+ha.replace('a', 'e')
+print(ha)
+          
